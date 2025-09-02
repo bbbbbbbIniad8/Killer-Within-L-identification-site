84c226f688283cbb381fb8b571c59b3e3195dcdc
0x7b <0x7b123@gmail.com>
2025-09-02 16:53:17 +0900

    機能追加

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const dict = {
 
 const people = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
 const personContainer = document.querySelector('.person');
+const KillerSelect = document.getElementById('Killer-select')
+const subKillerSelect = document.getElementById('sub-Killer-select')
+let whoKillerIndex = -1;
+let whosubKillerIndex = -1;
 
 people.forEach((personName, index) => { 
     const div = document.createElement('div');
@@ -20,22 +24,74 @@ people.forEach((personName, index) => {
 
     const checked = document.createElement('div'); 
     checked.id = `${personName}_checked`;
-    checked.className = 'person_checked';
+    checked.className = 'person_checked_before';
+    checked.textContent = 'Lの疑いアリ';
+
+    // const checkBox = document.createElement('input');
+    // checkBox.type = 'checkbox';
+    // checkBox.label = 'キラ陣営';
+    // checkBox.name = 'キラ陣営';
+
+    // const label = document.createElement('label');
+    // label.for = 'キラ陣営'
+    // label.textContent = 'キラ陣営か?'
+
     
     div.appendChild(innerElement);
     div.appendChild(checked);
+    // div.appendChild(checkBox)
+    // div.appendChild(label)
 
     div.addEventListener('click', () => {
         check_person(index); 
     });
     personContainer.appendChild(div);
+
+    const option = document.createElement('option');
+    option.value = index;
+    option.textContent = personName;
+    KillerSelect.appendChild(option);
+
+    const option2 = document.createElement('option');
+    option2.value = index;
+    option2.textContent = personName;
+    subKillerSelect.appendChild(option2);
 });
+
+
+function selectProcessKiller(event){
+    const index = Number(event.target.value);
+    if (index != -1){
+        check_person(index)
+        if (whoKillerIndex != -1 && whosubKillerIndex){
+            check_person(whoKillerIndex)
+        }
+        whoKillerIndex = index;
+    }
+}
+
+function selectProcessSubKiller(event){
+    const index = Number(event.target.value);
+    if (index != -1 && index != whoKillerIndex){
+        check_person(index)
+        if (whosubKillerIndex != -1){
+            check_person(whosubKillerIndex)
+        }
+        whosubKillerIndex = index;
+    }
+}
+
+KillerSelect.addEventListener('change', selectProcessKiller);
+subKillerSelect.addEventListener('change', selectProcessSubKiller);
+
 
 function check_person(index){
     const target = personState.checkList[index];
     personState.checkList[index] = (target === true) ? false : true;
     let element = document.getElementById(`${dict[index]}_checked`);
-    element.textContent = (target === true) ? "" : "checked";
+    element.className = (target === true) ? 'person_checked_before' : 'person_checked_after' ;
+    element.textContent = (target === true) ? 'Lの疑いアリ' : 'L以外 確定';
+
 }
 
 function judge_who_L() {
