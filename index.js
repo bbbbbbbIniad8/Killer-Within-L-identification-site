984dc2d6215b23e68478377bbbca704537415ef1
0x7b <x7b123@gmail.com>
2025-09-02 09:53:31 +0900

    修正

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const dict = {
     0: "A", 1: "B", 2: "C", 3: "D", 4: "E", 5: "F", 6: "G", 7: "H", 8: "I", 9: "J",
 }
 
+const people = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+const personContainer = document.querySelector('.person');
+
+people.forEach((personName, index) => {
+    const div = document.createElement('div');
+    div.id = personName;
+    div.addEventListener('click', () => {
+        check_person(index); 
+    });
+    personContainer.appendChild(div);
+});
+
 function check_person(index){
     const target = personState.checkList[index];
     personState.checkList[index] = (target === true) ? false : true;
